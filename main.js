5a99b9666c0d883e5a979dd20ce010c4b60c11f9
Jessaleem <aleejess@gmail.com>
2022-12-28 15:50:28 -0500

    feat: maping info

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=10';
+let container = document.getElementById('container');
+const list = document.createDocumentFragment();
+const div = document.createElement('div');
+div.classList.add('btnContainer');
+
+const button = document.createElement('button');
 
 // fetch(URL)
 //   .then((res) => res.json())
@@ -8,12 +14,33 @@ const URL = 'https://api.thecatapi.com/v1/images/search';
 //   });
 
 const fetchData = async () => {
-  const res = await fetch(URL);
+  const res = await fetch(API_URL);
   const data = await res.json();
-  console.log(data);
-  const img = document.querySelector('img');
-  img.src = data[0].url;
+  // console.log(data);
+  showData(data);
+  // const img = document.querySelector('img');
+  // img.src = data[0].url;
 };
 
-const button = document.querySelector('button');
-button.addEventListener('click', fetchData);
+const showData = (data) => {
+  data.map((info) => {
+    const image = document.createElement('img');
+    image.src = `${info.url}`;
+    image.alt = 'Cat photo';
+    image.classList.add('photo');
+
+    list.appendChild(image);
+    console.log(list);
+  });
+
+  container.appendChild(list);
+
+  button.classList.add('random');
+  button.innerText = 'New cat images';
+  button.addEventListener('click', fetchData);
+  div.appendChild(button);
+
+  container.appendChild(div);
+};
+
+fetchData();
